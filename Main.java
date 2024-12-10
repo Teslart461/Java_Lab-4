8cd84652b960d8509192b94bbe7038dc4cf64c4d
Teslart461 <apanteleev1001@gmail.com>
2024-12-10 21:12:33 +0700

    Update Main.java (CalculCal)

--- Main.java
+++ Main.java
@@ -14,5 +14,9 @@ public class Main {
         Menu menu = new Menu();
         System.out.println("Создание меню:");
         menu.input(scanner);
+
+        // Вычисляем общее количество калорий в меню
+        double totalCalories = menu.calculateCalories();
+        System.out.println("Общее количество калорий в меню: " + totalCalories + " ккал");
     }
 }
