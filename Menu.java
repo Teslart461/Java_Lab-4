44e0b9afcbbd81663f8b071b5628872d32183ca9
Teslart461 <apanteleev1001@gmail.com>
2024-12-10 21:06:25 +0700

    Update Menu.java and done it

--- Menu.java
+++ Menu.java
@@ -18,7 +18,15 @@ public class Menu {
                 .sum();
     }
 
-
+    public void input(Scanner scanner) {
+        System.out.print("Введите количество приемов пищи в меню: ");
+        int numMeals = scanner.nextInt();
+        scanner.nextLine(); // Очистка буфера
+        for (int i = 0; i < numMeals; i++) {
+            System.out.println("Прием пищи " + (i + 1) + ":");
+            Meal meal = new Meal();
+            meal.input(scanner);
+            addMeal(meal);
         }
     }
 }
